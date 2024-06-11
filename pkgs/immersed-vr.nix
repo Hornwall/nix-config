a0e8b94d62f368fdba5cbada76ec1e807189e682
Hannes Hornwall <hannes@hornwall.me>
2024-06-11 22:11:25 +0200

    Add desktop entry for immersed

--- pkgs/immersed-vr.nix
+++ pkgs/immersed-vr.nix
@@ -7,7 +7,7 @@
 }:
 let
   pname = "immersed";
-  version = "10.3.2";
+  version = "10.3.2_2";
   system = "x86_64-linux";
 
   src = fetchurl {
@@ -51,6 +51,10 @@ let
     libcanberra
   ];
 
+  appimageContents = appimageTools.extractType2 {
+    inherit pname version src;
+  };
+
   meta = with lib; {
     description = "A VR coworking platform";
     homepage = "https://immersed.com";
@@ -62,5 +66,12 @@ let
 in
   appimageTools.wrapType2 rec {
     inherit pname version src meta;
+
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/Immersed.desktop $out/share/applications/Immersed.desktop
+      install -m 444 -D ${appimageContents}/Immersed.png $out/share/pixmaps/Immersed.png
+      substituteInPlace $out/share/applications/Immersed.desktop --replace 'Exec=Immersed' 'Exec=${pname}'
+    '';
+
     name = "${pname}-${version}";
   }
