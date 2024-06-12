af7517a62a5c2b7736a14566e2074c45dcd3ba86
Hannes Hornwall <hannes@hornwall.me>
2024-06-12 16:58:44 +0200

    Configure nvim tree

--- home-manager/dotfiles/config/nvim/lua/plugins/configs/nvimtree.lua
+++ home-manager/dotfiles/config/nvim/lua/plugins/configs/nvimtree.lua
@@ -1,13 +1,22 @@
 nvimtree = require("nvim-tree")
 
+local function my_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+  -- custom mappings
+  vim.keymap.set('', '-', api.tree.toggle, opts(""))
+end
+
 nvimtree.setup {
   view = {
-    --mappings = {
-    --  list = {
-    --    { key = "-", action = "" },
-    --  },
-    --},
   },
+  on_attach = my_on_attach,
   update_focused_file = {
     enable = true,
   },
