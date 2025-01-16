3f0e14bdf39ae20b604999152ba1f6f31f4a0eb6
Hannes Hornwall <hannes@hornwall.me>
2025-01-16 17:02:28 +0100

    Update nix packages

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -155,6 +155,7 @@
       st = "status -sb";
       lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
       fixup = "!git log -n 50 --pretty=format:'%h %s' --no-merges | fzf | cut -c -7 | xargs -o git commit --fixup";
+      squash = "!f() { git fetch origin main && git rebase -i --autosquash $(git merge-base main HEAD); }; f";
     };
   };
 }
