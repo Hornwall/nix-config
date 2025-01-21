71754d60f54cc68621a0eb6df2d1c465ca0a88ed
Hannes Hornwall <hannes@hornwall.me>
2025-01-21 13:30:04 +0100

    Update flake.lock

--- nixos/thinkpad-z16/configuration.nix
+++ nixos/thinkpad-z16/configuration.nix
@@ -102,7 +102,21 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
+    wireplumber.extraConfig."10-bluez" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "hsp_hs"
+          "hsp_ag"
+          "hfp_hf"
+          "hfp_ag"
+        ];
+      };
+    };
   };
+
 
   # Enable keybase
   services.keybase.enable = true;
