6b372fb5177d5bac77d4d95e79a65c37d8947d76
Hannes Hornwall <hannes@hornwall.me>
2025-05-25 14:03:05 +0200

    Update immersed

--- pkgs/immersed-vr.nix
+++ pkgs/immersed-vr.nix
@@ -12,7 +12,7 @@ let
 
   src = fetchurl {
     url = "https://static.immersed.com/dl/Immersed-x86_64.AppImage";
-    hash = "sha256-U0YYXai1zUeU8ZaIb7QiCrpbfvOAYywWgNQ3r2uVyl0=";
+    hash = "sha256-5VEaNoH5+W9MA6voSufqQeMLRU4z9dXqGMDMU0+Gagc=";
   };
 
   extraPkgs = pkgs: with pkgs; [
