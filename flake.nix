ff3b424090715e4fe924a6c7fd0c319ab6592a13
Hannes Hornwall <hannes@hornwall.me>
2025-12-05 18:30:17 +0100

    Update home-manager

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     };
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = {
