a652603e1c7dfa2c4ed7e9f9593c196c6b761f3b
Hannes Hornwall <hannes@hornwall.me>
2026-02-15 16:45:57 +0100

    Use open nvidia drivers

--- nixos/allakazam/configuration.nix
+++ nixos/allakazam/configuration.nix
@@ -96,7 +96,7 @@
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
