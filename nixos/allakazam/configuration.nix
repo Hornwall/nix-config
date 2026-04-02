434929aa6252b62135f2f95661abce4293c2d068
Hannes Hornwall <hannes@hornwall.me>
2026-04-02 16:41:43 +0200

    Update kernel

--- nixos/allakazam/configuration.nix
+++ nixos/allakazam/configuration.nix
@@ -34,7 +34,7 @@
   };
 
   # Use latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.kernelPackages = pkgs.linuxPackages_6_19;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
