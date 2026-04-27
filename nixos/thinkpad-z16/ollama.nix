447c4a2d2d717d09d989facbf47082bf622ab030
Hannes Hornwall <hannes@hornwall.me>
2026-04-27 11:08:03 +0200

    Add openwebui

--- nixos/thinkpad-z16/ollama.nix
+++ nixos/thinkpad-z16/ollama.nix
@@ -4,12 +4,14 @@
     enable = true;
     package = pkgs.unstable.ollama;
     loadModels = ["llama3.2:3b"];
-    acceleration = "rocm";
+    acceleration = "vulkan";
     environmentVariables = {
       HCC_AMDGPU_TARGET = "gfx1103"; # used to be necessary, but doesn't seem to anymore
     };
     rocmOverrideGfx = "11.0.2";
   };
+
+  services.open-webui.enable = true;
 
   #services.open-webui = {
   #  enable = true;
